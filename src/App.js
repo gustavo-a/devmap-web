0d7a38c73b5ae496597658454a70552000944e61
Gustavo <gustavo@wyasi.com.br>
2020-01-17 01:24:21 -0300

    Interface web concluída

--- src/App.js
+++ src/App.js
@@ -1,86 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([])
+
+  useEffect(()=>{
+    async function loadDevs(){
+      const response =  await api.get('/devs')
+      setDevs(response.data)
+    }
+    loadDevs()
+  }, [])
+
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data)
+    setDevs([...devs, response.data])
+  }
+
   return (
    <div id="app">
      <aside>
       <strong>Cadastrar</strong>
-      <form>
-        <div className="input-block">
-          <label htmlFor="github_username">Usuário do Github</label>
-          <input name="github_username" type="text" id="github_username" required />
-        </div>
-
-        <div className="input-block">
-          <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" type="text" id="techs" required />
-        </div>
-
-        <div className="input-group">
-          <div className="input-block">
-            <label htmlFor="latitude">Latitude</label>
-            <input name="latitude" type="text" id="latitude" required />  
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="longitude">Longitude</label>
-            <input name="longitude" type="text" id="longitude" required />  
-          </div>
-        </div>
-        
-        <button type="submit">Salvar</button>
-      </form>
+      <DevForm onSubmit={handleAddDev} />
      </aside>
      <main>
       <ul>
-        <li className="dev-item">
-          <header>
-            <img src="https://placedog.net/300/300?random" alt=""/>
-            <div className="user-info">
-              <strong>Gustavo Rocha</strong>
-              <span>Wordpress, VueJs, ReactJS</span>
-            </div>
-          </header>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore earum qui repellendus error modi, mollitia reprehenderit dolorem ab hic voluptatibus.</p>
-          <a href="https://github.com/gustavo-a">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://placedog.net/300/300?random" alt=""/>
-            <div className="user-info">
-              <strong>Gustavo Rocha</strong>
-              <span>Wordpress, VueJs, ReactJS</span>
-            </div>
-          </header>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore earum qui repellendus error modi, mollitia reprehenderit dolorem ab hic voluptatibus.</p>
-          <a href="https://github.com/gustavo-a">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://placedog.net/300/300?random" alt=""/>
-            <div className="user-info">
-              <strong>Gustavo Rocha</strong>
-              <span>Wordpress, VueJs, ReactJS</span>
-            </div>
-          </header>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore earum qui repellendus error modi, mollitia reprehenderit dolorem ab hic voluptatibus.</p>
-          <a href="https://github.com/gustavo-a">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://placedog.net/300/300?random" alt=""/>
-            <div className="user-info">
-              <strong>Gustavo Rocha</strong>
-              <span>Wordpress, VueJs, ReactJS</span>
-            </div>
-          </header>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore earum qui repellendus error modi, mollitia reprehenderit dolorem ab hic voluptatibus.</p>
-          <a href="https://github.com/gustavo-a">Acessar perfil no Github</a>
-        </li>
+        {devs.map( dev => (
+          <DevItem key={dev._id} dev={dev} />
+        ))}
       </ul>
      </main>
    </div>
